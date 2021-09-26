de0a1cc1979da77d0a4552be580535746a8c64ce
a7b8 <a7b8@meta.ua>
2021-09-26 23:00:15 +0300

    be found emoji  number

---
+++ 2/app.py
@@ -0,0 +1,23 @@
+import emoji, requests
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+@app.route("/test")
+def test():
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+#print(count * (animal + ' say moooo\n'))
+#"\U0001F418"
+#"\U0001F404"
